72beb15ff08887cf19df0fca69d38aaf57a0bb23
supriyo404 <er.supriya93@gmail.com>
2021-08-07 21:02:52 +0530

    create separate package for repo
    create separate package for repo

--- src/main/java/com/supriyo/api/Service/MarketService.java
+++ src/main/java/com/supriyo/api/Service/MarketService.java
@@ -3,8 +3,8 @@ package com.supriyo.api.Service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.supriyo.api.MarketRepo;
 import com.supriyo.api.model.Market;
+import com.supriyo.api.repo.MarketRepo;
 
 @Service
 public class MarketService {
--- src/main/java/com/supriyo/api/repo/MarketRepo.java
+++ src/main/java/com/supriyo/api/repo/MarketRepo.java
@@ -1,10 +1,8 @@
-package com.supriyo.api;
+package com.supriyo.api.repo;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import com.supriyo.api.model.Market;
 
 
